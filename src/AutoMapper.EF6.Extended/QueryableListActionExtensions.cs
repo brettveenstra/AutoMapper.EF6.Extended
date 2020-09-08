48dbc0f498d16270fff46f80013dc817054d7371
PrinceOwen9466 <princeowenolive@gmail.com>
2020-09-08 01:08:18 +0100

    Ported to .NET Standard 2.0

--- src/AutoMapper.EF6.Extended/QueryableListActionExtensions.cs
+++ src/AutoMapper.EF6.Extended/QueryableListActionExtensions.cs
@@ -1,12 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data.Entity;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using AutoMapper.QueryableExtensions;
 using DelegateDecompiler;
-using DelegateDecompiler.EntityFramework;
+using DelegateDecompiler.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 
 namespace AutoMapper.EF6.Extended
 {
